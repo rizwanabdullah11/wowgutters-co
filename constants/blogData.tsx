8ed73a097859786f2c51adf2598a6e88622820bb
rizwanabdullah11 <rizwan.abdullah193.com>
2026-07-13 19:58:27 +0500

    here I created a new blogs autumn before removed a dribs

--- constants/blogData.tsx
+++ constants/blogData.tsx
@@ -18,11 +18,13 @@ import { sellyOakBlogPost } from './sellyOakBlog';
 import { summerGutterCleaningBlogPost } from './summerGutterCleaningBlog';
 import { yardleyBlogPost } from './yardleyBlog';
 import { birminghamGutterMaintenanceCalendarBlogPost } from './birminghamGutterMaintenanceCalendarBlog';
+import { birminghamAutumnLeavesBlogPost } from './birminghamAutumnLeavesBlog';
 export type { BlogPost } from './blogTypes';
 import type { BlogPost } from './blogTypes';
 
 export const blogPosts: BlogPost[] = [
   birminghamGutterMaintenanceCalendarBlogPost,
+  birminghamAutumnLeavesBlogPost,
   cleanGuttersProtectBirminghamBlogPost,
   professionalGutterCleaningProtectsBlogPost,
   earlySignsBlockedGuttersBirminghamBlogPost,
