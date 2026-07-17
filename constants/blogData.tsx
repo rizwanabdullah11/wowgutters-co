9063d716afae5a0e86952115f78d42c49ef6fe59
rizwanabdullah11 <rizwan.abdullah193.com>
2026-07-17 19:06:16 +0500

    here I created late summer gutter cleaning

--- constants/blogData.tsx
+++ constants/blogData.tsx
@@ -21,11 +21,13 @@ import { birminghamGutterMaintenanceCalendarBlogPost } from './birminghamGutterM
 import { birminghamAutumnLeavesBlogPost } from './birminghamAutumnLeavesBlog';
 import { birminghamGutterHealthCheckBlogPost } from './birminghamGutterHealthCheckBlog';
 import { professionalGutterCleaningExtendsRooflineLifeBlogPost } from './professionalGutterCleaningExtendsRooflineLifeBlog';
+import { lateSummerGutterMaintenanceBlogPost } from './lateSummerGutterMaintenanceBlog';
 import { yearRoundGutterProtectionBlogPost } from './yearRoundGutterProtectionBlog';
 export type { BlogPost } from './blogTypes';
 import type { BlogPost } from './blogTypes';
 
 export const blogPosts: BlogPost[] = [
+  lateSummerGutterMaintenanceBlogPost,
   yearRoundGutterProtectionBlogPost,
   professionalGutterCleaningExtendsRooflineLifeBlogPost,
   birminghamGutterHealthCheckBlogPost,
