126621770f9664b5a58f848577f52cab740e4e31
rizwanabdullah11 <rizwan.abdullah193.com>
2026-07-31 20:30:13 +0500

    here I created a new blog west-midlands-water-damage

--- constants/blogData.tsx
+++ constants/blogData.tsx
@@ -31,10 +31,12 @@ import { guttersTellingYouSummerBlogPost } from './guttersTellingYouSummerBlog';
 import { warehouseBoxGutterBlogPost } from './warehouseBoxGutterBlog';
 import { birminghamHomeownersForgetSummerBlogPost } from './birminghamHomeownersForgetSummerBlog';
 import { birminghamHomeownersMissSummerBlogPost } from './birminghamHomeownersMissSummerBlog';
+import { westMidlandsHomeStoringWaterDamageBlogPost } from './westMidlandsHomeStoringWaterDamageBlog';
 export type { BlogPost } from './blogTypes';
 import type { BlogPost } from './blogTypes';
 
 export const blogPosts: BlogPost[] = [
+  westMidlandsHomeStoringWaterDamageBlogPost,
   birminghamHomeownersMissSummerBlogPost,
   birminghamHomeownersForgetSummerBlogPost,
   warehouseBoxGutterBlogPost,
