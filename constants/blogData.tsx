439755cc93f1de08108a38662ccd59166969867d
rizwanabdullah11 <rizwan.abdullah193.com>
2026-08-04 21:26:44 +0500

    here I created a new blog Professional Gutter Walkthrough

--- constants/blogData.tsx
+++ constants/blogData.tsx
@@ -33,10 +33,12 @@ import { birminghamHomeownersForgetSummerBlogPost } from './birminghamHomeowners
 import { birminghamHomeownersMissSummerBlogPost } from './birminghamHomeownersMissSummerBlog';
 import { westMidlandsHomeStoringWaterDamageBlogPost } from './westMidlandsHomeStoringWaterDamageBlog';
 import { canYouCleanYourOwnGuttersBlogPost } from './canYouCleanYourOwnGuttersBlog';
+import { professionalGutterCleanWalkthroughBlogPost } from './professionalGutterCleanWalkthroughBlog';
 export type { BlogPost } from './blogTypes';
 import type { BlogPost } from './blogTypes';
 
 export const blogPosts: BlogPost[] = [
+  professionalGutterCleanWalkthroughBlogPost,
   canYouCleanYourOwnGuttersBlogPost,
   westMidlandsHomeStoringWaterDamageBlogPost,
   birminghamHomeownersMissSummerBlogPost,
