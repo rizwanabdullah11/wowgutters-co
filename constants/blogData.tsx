a81c4213755042e151a64d07245a7f26a9f30666
rizwanabdullah11 <rizwan.abdullah193.com>
2026-08-17 18:45:05 +0500

    here I created an wind direction blog

--- constants/blogData.tsx
+++ constants/blogData.tsx
@@ -39,10 +39,12 @@ import { treeCoverGutterScheduleBlogPost } from './treeCoverGutterCleaningSchedu
 import { mossOnYourRoofBirminghamGuttersBlogPost } from './mossOnYourRoofBirminghamGuttersBlog';
 import { blockManagementGutterMaintenanceBlogPost } from './blockManagementGutterMaintenanceBlog';
 import { soffitDamageWestMidlandsEarlySignsBlogPost } from './soffitDamageWestMidlandsEarlySignsBlog';
+import { windDirectionDebrisBuildupBlogPost } from './windDirectionDebrisBuildupBlog';
 export type { BlogPost } from './blogTypes';
 import type { BlogPost } from './blogTypes';
 
 export const blogPosts: BlogPost[] = [
+  windDirectionDebrisBuildupBlogPost,
   soffitDamageWestMidlandsEarlySignsBlogPost,
   blockManagementGutterMaintenanceBlogPost,
   mossOnYourRoofBirminghamGuttersBlogPost,
